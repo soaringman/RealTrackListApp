d6d6cc58342af952b4abf1dc7d04820944e182f6
Alexey Gulyaev <ghost-shadows@yandex.ru>
2021-10-14 16:06:48 +0300

    Initial Commit

---
+++ RealTrackListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTrackListApp
+//
+//  Created by Алексей Гуляев on 14.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
